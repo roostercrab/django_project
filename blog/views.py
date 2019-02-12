715718b2def087fb81a9e2056d3487bcfad4cae6
Jesse <jesse.shea@qixingdesign.com>
2019-02-11 16:53:33 -0800

    stuff

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
 from .models import Post
-posts =  [
-    {
-        'author': 'Jesse Shea',
-        'title': 'WTF',
-        'content': 'Is i Learning?',
-        'date_posted': 'yesterday'
-    },
-    {
-        'author': 'Jimmy Pop',
-        'title': 'Tickle sandwich',
-        'content': 'DEEERRPPPP',
-        'date_posted': 'next year'
-    }
-]
+
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
